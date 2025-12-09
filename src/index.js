cdb806e6b56af5beae0e516c7fdb91fa74bd1b9d
sanddot <sanginta@gmail.com>
2025-12-08 17:13:53 -0800

    Added basic form handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+function generatePoem(event) {
+  event.preventDefault();
+  //alert("Generating poem");
 
+  let poemAIText = document.querySelector("#poem");
+  poemAIText.innerHTML = "This is the poem!";
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
